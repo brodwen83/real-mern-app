f939750b36323614fe2e9b722681c7ad96db7c7c
wendell <brodwen83@gmail.com>
2018-12-12 09:38:48 +0800

    MongoDB MLab Connected

--- src/server.js
+++ src/server.js
@@ -1,6 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+// MongoDB connection
+mongoose
+  .connect(
+    process.env.MONGODB_URL,
+    { useNewUrlParser: true, useCreateIndex: true }
+  )
+  .then(() => console.log("MongoDB on MLab connected..."))
+  .catch(err => console.log("Can't connect to the database", err));
+/** end MongoDB connection. */
 
 app.get("/", (req, res) => res.send("Hello!"));
 
