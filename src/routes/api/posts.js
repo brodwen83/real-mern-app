f6190c3c6b41e783335980a755da1bd0a6b57cf2
wendell <brodwen83@gmail.com>
2018-12-14 09:30:37 +0800

    Likes and Dislikes post

--- src/routes/api/posts.js
+++ src/routes/api/posts.js
@@ -78,7 +78,7 @@ router.delete(
   "/:id",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    Profile.find({ user: req.user.id }).then(profile => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
       Post.findById(req.params.id)
         .then(post => {
           // Check post owner
@@ -89,6 +89,71 @@ router.delete(
 
           // Delete
           post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "Post not found" }));
+    });
+  }
+);
+
+/**
+ *  @route        POST api/posts/unlike/:id
+ *  @description  Unlikes post
+ *  @access       Private
+ */
+router.post(
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length === 0
+          )
+            return res
+              .status(400)
+              .json({ notliked: "You have not yet liked the post" });
+
+          // Get the remove index
+          const removeIndex = post.likes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id);
+
+          // Splice out of the array
+          post.likes.splice(removeIndex, 1);
+
+          // Save
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "Post not found" }));
+    });
+  }
+);
+
+/**
+ *  @route        POST api/posts/like/:id
+ *  @description  Likes post
+ *  @access       Private
+ */
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          )
+            return res
+              .status(400)
+              .json({ alreadyliked: "User already like this post" });
+
+          // Add user id to likes array
+          post.likes.unshift({ user: req.user.id });
+          post.save().then(post => res.json(post));
         })
         .catch(err => res.status(404).json({ postnotfound: "Post not found" }));
     });
