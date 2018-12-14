03b77bc91965588fe40971e13a75649d907b8cd9
wendell <brodwen83@gmail.com>
2018-12-14 08:27:38 +0800

    Fetch all Posts and Post by ID

--- src/routes/api/posts.js
+++ src/routes/api/posts.js
@@ -17,6 +17,29 @@ router.get("/test", (req, res) => res.json({ message: "User's Posts" }));
 
 /**
  *  @route        GET api/posts
+ *  @description  Get post
+ *  @access       Public
+ */
+router.get("/", (req, res) => {
+  Post.find({})
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ noposts: "Error Fetching posts" }));
+});
+
+/**
+ *  @route        GET api/posts/:id
+ *  @description  Get post by id
+ *  @access       Public
+ */
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopost: "Post not found" }));
+});
+
+/**
+ *  @route        GET api/posts
  *  @description  Create post
  *  @access       Public
  */
