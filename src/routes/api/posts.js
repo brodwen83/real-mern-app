1ef6164558d282578fc0261d13c1acee8d80d956
wendell <brodwen83@gmail.com>
2018-12-14 09:48:50 +0800

    Post a comment

--- src/routes/api/posts.js
+++ src/routes/api/posts.js
@@ -160,4 +160,39 @@ router.post(
   }
 );
 
+/** Comments route handlers */
+
+/**
+ *  @route        POST api/posts/comment/:id
+ *  @description  Add commment to a post
+ *  @access       Private
+ */
+router.post(
+  "/comment/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInputs(req.body);
+    // Check Validation
+    if (!isValid)
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+
+        // Add to comments  array
+        post.comments.unshift(newComment);
+        // Save post
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "Post not found" }));
+  }
+);
+
 export default router;
